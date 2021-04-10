765b7f8b80db01a3998d8b1c08cf787d19025e31
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-04-10 19:01:18 -0400

    Create login view, set up routing and authentication token

---
+++ friends/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
+import Login from './components/Login/Login';
+import FriendsList from './components/FriendsList/FriendsList';
+
+import './App.css';
+
+function App() {
+  const logout = () => {
+    localStorage.removeItem('loginToken');
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/login" onClick={logout}>Logout</Link>
+          </li>
+          <li>
+            <Link to="/friends">Friends</Link>
+          </li>
+        </ul>
+        <Switch>
+          <ProtectedRoute exact path="/friends" component={FriendsList} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ friends/src/components/Login/Login.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import {axiosWithAuth} from '../../utils/axiosWithAuth';
+import {useHistory} from 'react-router-dom';
+
+const initialCredentials = {
+    username: '',
+    password: ''
+}
+
+const Login = () => {
+    const [credentials, setCredentials] = useState(initialCredentials);
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setCredentials({
+            ...credentials,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const login = (event) => {
+        event.preventDefault();
+        axiosWithAuth().post("http://localhost:5000/api/login", credentials)
+        .then(response => {
+            console.log(response)
+            localStorage.setItem('loginToken', response.data.payload)
+            history.push("/friends");
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    }
+
+    return (
+        <div>
+            <form onSubmit={login}>
+                <label>
+                    Username
+                    <input 
+                        type="text"
+                        name="username"
+                        value={credentials.username}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Password
+                    <input 
+                        type="password"
+                        name="password"
+                        value={credentials.password}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button>Log in</button>
+            </form>
+        </div>
+    );
+}
+ 
+export default Login;
